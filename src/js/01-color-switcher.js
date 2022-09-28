1ad429ea8b4569b75250f61efdc4a9bc4d32d7f0
EugeneDemchenko <108480374+EugeneDemchenko@users.noreply.github.com>
2022-09-28 13:01:35 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+const view = document.querySelector('body');
+let timerId = null;
+
+stopButton.setAttribute('disabled', '')
+
+startButton.addEventListener('click', colorSwitcherOn)
+stopButton.addEventListener('click', colorSwitcherOff)
+
+function colorSwitcherOn() {
+    timerID = setInterval(() => {
+       view.style.background = getRandomHexColor() 
+    }, 1000);
+    stopButton.removeAttribute('disabled')
+    startButton.setAttribute('disabled', '')
+    console.log(timerId);
+}
+function colorSwitcherOff() {
+    clearInterval(timerId);
+    startButton.removeAttribute('disabled')
+    stopButton.setAttribute('disabled', '')
+}
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
